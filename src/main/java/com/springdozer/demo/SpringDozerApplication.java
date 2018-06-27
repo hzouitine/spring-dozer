fe88bd45c3a4578a43095908d2daee233a22f18c
Hamza Zouitine <3zouitine@gmail.com>
2018-06-27 22:22:55 +0100

    Init project ( Adding dependencies )

---
+++ src/main/java/com/springdozer/demo/SpringDozerApplication.java
@@ -0,0 +1,12 @@
+package com.springdozer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDozerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDozerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/springdozer/demo/SpringDozerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springdozer.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDozerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
